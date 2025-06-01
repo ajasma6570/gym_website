1a8bdf1646d527bcbba4794780a62414ef6c7fbd
ajasma6570 <ajasmaprince@gmail.com>
2025-06-02 00:18:10 +0530

    layout structure updated

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,16 +1,24 @@
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+import Map from "@/components/Map";
+
 export default function About() {
   return (
-    <div className="min-h-screen  pt-20 py-8">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="text-center">
-          <h1 className="text-4xl font-bold text-red-600 mb-6">About Us</h1>
-          <p className="text-lg  max-w-3xl mx-auto">
-            Welcome to our gym! We are dedicated to helping you achieve your
-            fitness goals with state-of-the-art equipment, experienced trainers,
-            and a supportive community.
-          </p>
+    <>
+      <div className="min-h-screen flex flex-col justify-center items-center pt-20 py-8">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+          <div className="text-center">
+            <h1 className="text-4xl font-bold text-red-600 mb-6">
+              Page Under Construction
+            </h1>
+            <p className="text-lg max-w-3xl mx-auto">
+              We’re working hard to bring you an amazing experience! This page
+              is currently under construction. Check back soon for updates and
+              exciting content.
+            </p>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,10 @@
-import Footer from "@/components/Footer";
-import Header from "@/components/Header";
 import Home from "@/components/Home";
 import React from "react";
 
 export default function page() {
   return (
     <>
-      <Header />
       <Home />
-      <Footer />
     </>
   );
 }
