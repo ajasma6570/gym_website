1ab5daec1994eba3db75bde9987fb53a9f5e4a65
Aleena Vincent <aleenavincent4u@gmail.com>
2025-06-12 11:49:58 -0400

    Connected neon db - member creation

---
+++ src/app/api/member/route.ts
@@ -0,0 +1,19 @@
+import { prisma } from "@/lib/prisma";
+import { NextResponse } from "next/server";
+
+export async function POST(request: Request) {
+  const { name, age, weight, height, contactNo, payment } = await request.json();
+
+  const member = await prisma.member.create({
+    data: {
+      name,
+      age,
+      weight,
+      height,
+      contactNo,
+      payment,
+    },                                                                                              
+  });
+
+  return NextResponse.json(member, { status: 201 });
+}
